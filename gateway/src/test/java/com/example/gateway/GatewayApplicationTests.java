dcf3f857136ae983546b18c6bde17531fe476c4c
soshal <111147773+soshal@users.noreply.github.com>
2025-01-05 23:15:07 +0530

    config-server and api gateway added

--- gateway/src/test/java/com/example/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/com/example/gateway/GatewayApplicationTests.java
@@ -1,16 +1,13 @@
-package com.example.jobservice;
+package com.example.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JobserviceApplicationTests {
+class GatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
 }
-
-
-
